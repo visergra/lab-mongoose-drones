288e044431805fd1719f3fa9386012439cbe7209
visergra <visergra@hotmail.com>
2017-06-24 14:50:27 +0200

    list and new with styles

--- starter-code/routes/drones.js
+++ starter-code/routes/drones.js
@@ -3,19 +3,39 @@ const express = require('express');
 // require the Drone model here
 
 const router = express.Router();
-
+const Drone = require('../models/drone');
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2
+  Drone.find({}, (err, drones) => {
+    if (err) {
+      return next(err)
+    }
+    res.render('drones/index', {
+      drones: drones
+    });
+  });
 });
 
 
 router.get('/drones/new', (req, res, next) => {
   // Iteration #3
+  res.render('drones/new');
 });
 
 router.post('/drones', (req, res, next) => {
   // Iteration #3
+  let droneInfo = {
+    droneName: req.body.droneName,
+    propellers: req.body.propellers,
+    maxSpeed: req.body.maxSpeed
+  };
+
+  const newDrone = new Drone(droneInfo);
+  newDrone.save((err) => {
+    return next(err);
+  });
+  return res.redirect('/drones');
 });
 
 module.exports = router;
